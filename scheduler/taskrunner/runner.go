ac7de0eee22ef2ce082b01b9a1e78d5eedfdf7f7
charles <wanchuan21@gmail.com>
2021-07-11 21:30:10 +0800

    new

---
+++ scheduler/taskrunner/runner.go
@@ -0,0 +1,63 @@
+package taskrunner
+
+type Runner struct {
+	Controller controlChan
+	Error      controlChan
+	Data       dataChan
+	dataSize   int
+	longLived  bool
+	Dispatcher fn
+	Executor   fn
+}
+
+func NewRunner(size int, longlived bool, d, e fn) *Runner {
+	return &Runner{
+		Controller: make(chan string, 1),
+		Error:      make(chan string, 1),
+		Data:       make(chan interface{}, size),
+		longLived:  longlived,
+		dataSize:   size,
+		Dispatcher: d,
+		Executor:   e,
+	}
+}
+
+func (r *Runner) startDispatcher() {
+	defer func() {
+		if !r.longLived {
+			close(r.Controller)
+			close(r.Data)
+			close(r.Error)
+		}
+	}()
+
+	for {
+		select {
+		case c := <-r.Controller:
+			if c == READY_TO_DISPATCH {
+				if err := r.Dispatcher(r.Data); err != nil {
+					r.Error <- CLOSE
+				} else {
+					r.Controller <- READY_TO_EXECUTE
+				}
+			}
+
+			if c == READY_TO_EXECUTE {
+				if err := r.Executor(r.Data); err != nil {
+					r.Error <- CLOSE
+				} else {
+					r.Controller <- READY_TO_DISPATCH
+				}
+			}
+		case e := <-r.Error:
+			if e == CLOSE {
+				return
+			}
+		}
+	}
+}
+
+func (r *Runner) StartAll() {
+	r.Controller <- READY_TO_DISPATCH
+	r.startDispatcher()
+}
